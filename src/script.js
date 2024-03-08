c83aec963e4a09c36e8a95470f757780ed0ce7e7
Rosen Dimov <rosen.y.dimov@gmail.com>
2024-03-08 12:46:37 +0200

    Finish timer

--- src/script.js
+++ src/script.js
@@ -1,22 +1,28 @@
 const MAX_SECONDS = 60;
 const MAX_MINUTES = 60;
 const MAX_HOURS = 24;
+const ZERO_STR = "00";
 
 const timerCallback = (function () {
-  // const currDay = document.querySelector(".day-wrapper .current");
-  // const currDayVal = currDay.querySelector(".count");
-  // const nextDay = document.querySelector(".seconds-wrapper .next");
-  // const nextDayVal = nextSec.querySelector(".count");
+  const currDaysEl = document.querySelector(".day-wrapper .current");
+  const currDaysValEl = currDaysEl.querySelector(".count");
+  const nextDaysEl = document.querySelector(".day-wrapper .next");
+  const nextDaysValEl = nextDaysEl.querySelector(".count");
 
-  const currMin = document.querySelector(".minutes-wrapper .current");
-  const currMinVal = currMin.querySelector(".count");
-  const nextMin = document.querySelector(".minutes-wrapper .next");
-  const nextMinVal = nextMin.querySelector(".count");
+  const currHoursEl = document.querySelector(".hours-wrapper .current");
+  const currHoursValEl = currHoursEl.querySelector(".count");
+  const nextHoursEl = document.querySelector(".hours-wrapper .next");
+  const nextHoursValEl = nextHoursEl.querySelector(".count");
 
-  const currSec = document.querySelector(".seconds-wrapper .current");
-  const currSecVal = currSec.querySelector(".count");
-  const nextSec = document.querySelector(".seconds-wrapper .next");
-  const nextSecVal = nextSec.querySelector(".count");
+  const currMinsEl = document.querySelector(".minutes-wrapper .current");
+  const currMinsValEl = currMinsEl.querySelector(".count");
+  const nextMinsEl = document.querySelector(".minutes-wrapper .next");
+  const nextMinsValEl = nextMinsEl.querySelector(".count");
+
+  const currSecEl = document.querySelector(".seconds-wrapper .current");
+  const currSecValEl = currSecEl.querySelector(".count");
+  const nextSecEl = document.querySelector(".seconds-wrapper .next");
+  const nextSecValEl = nextSecEl.querySelector(".count");
 
   function toValueString(value) {
     return value < 10 ? `0${value}` : String(value);
@@ -26,50 +32,145 @@ const timerCallback = (function () {
     return html[0] === "0" ? +html[1] : +html;
   }
 
-  function getSeconds(html) {
-    const seconds = html[0] === "0" ? +html[1] : +html;
-    const newValue = seconds === 0 ? MAX_SECONDS - 1 : seconds - 1;
+  function getNewValue(current, max) {
+    const currentNum = trimLeadingZero(current);
+    const newValue = currentNum === 0 ? max - 1 : currentNum - 1;
     return newValue;
+  }
+
+  function getDays(html, minutes) {
+    if (typeof minutes !== "undefined" && minutes !== MAX_HOURS - 1) {
+      return { update: false };
+    }
+    const currentNum = trimLeadingZero(html);
+    return currentNum - 1;
+  }
+
+  function getHours(html, minutes) {
+    if (typeof minutes !== "undefined" && minutes !== MAX_MINUTES - 1) {
+      return { update: false };
+    }
+    return getNewValue(html, MAX_HOURS);
   }
 
   function getMinutes(html, seconds) {
-    if (typeof seconds !== 'undefined' && seconds !== 0) { return { update: false }; }
-
-    const minutes = trimLeadingZero(html);
-    const newValue = minutes === 0 ? MAX_MINUTES - 1 : minutes - 1;
-    return newValue;
+    if (typeof seconds !== "undefined" && seconds !== MAX_SECONDS - 1) {
+      return { update: false };
+    }
+    return getNewValue(html, MAX_MINUTES);
   }
-  
-  function handleMinutes(seconds) {
-    const nextMins = getMinutes(currMinVal.innerHTML, seconds);
 
-    if (nextMins?.update === false) { return nextMins; }
+  function getSeconds(html) {
+    return getNewValue(html, MAX_SECONDS);
+  }
+
+  function handleDays(hours) {
+    const nextDays = getDays(currDaysValEl.innerHTML, hours);
+
+    if (nextDays?.update === false) {
+      return;
+    }
+
+    const nextDaysHtml = toValueString(nextDays);
+    const afterNextDays = getMinutes(nextDaysHtml);
+    const afterNextDaysHtml = toValueString(afterNextDays);
+
+    animate(
+      currDaysEl,
+      nextDaysEl,
+      currDaysValEl,
+      nextDaysValEl,
+      nextDaysHtml,
+      afterNextDaysHtml
+    );
+  }
+
+  function handleHours(minutes) {
+    const nextHours = getHours(currHoursValEl.innerHTML, minutes);
+
+    if (nextHours?.update === false) {
+      return nextHours;
+    }
+
+    const nextHoursHtml = toValueString(nextHours);
+    const afterNextHours = getMinutes(nextHoursHtml);
+    const afterNextHoursHtml = toValueString(afterNextHours);
+
+    animate(
+      currHoursEl,
+      nextHoursEl,
+      currHoursValEl,
+      nextHoursValEl,
+      nextHoursHtml,
+      afterNextHoursHtml
+    );
+
+    return { hours: nextHours, update: true };
+  }
+
+  function handleMinutes(seconds) {
+    const nextMins = getMinutes(currMinsValEl.innerHTML, seconds);
+
+    if (nextMins?.update === false) {
+      return nextMins;
+    }
 
     const nextMinsHtml = toValueString(nextMins);
     const afterNextMins = getMinutes(nextMinsHtml);
     const afterNextMinsHtml = toValueString(afterNextMins);
 
-    handleAnimation(currMin, nextMin, currMinVal, nextMinVal, nextMinsHtml, afterNextMinsHtml);
-    
+    animate(
+      currMinsEl,
+      nextMinsEl,
+      currMinsValEl,
+      nextMinsValEl,
+      nextMinsHtml,
+      afterNextMinsHtml
+    );
+
     return { minutes: nextMins, update: true };
   }
 
   function handleSeconds() {
-    const nextSecs = getSeconds(currSecVal.innerHTML);
+    if (
+      currSecValEl.innerHTML === ZERO_STR &&
+      currMinsValEl.innerHTML === ZERO_STR &&
+      currHoursValEl.innerHTML === ZERO_STR &&
+      currDaysEl.innerHTML === ZERO_STR
+    ) {
+      clearInterval(intervalId);
+      return {};
+    }
+
+    const nextSecs = getSeconds(currSecValEl.innerHTML);
     const nextSecsHtml = toValueString(nextSecs);
     const afterNextSecs = getSeconds(nextSecsHtml);
     const afterNextSecsHtml = toValueString(afterNextSecs);
 
-    handleAnimation(currSec, nextSec, currSecVal, nextSecVal, nextSecsHtml, afterNextSecsHtml);
+    animate(
+      currSecEl,
+      nextSecEl,
+      currSecValEl,
+      nextSecValEl,
+      nextSecsHtml,
+      afterNextSecsHtml
+    );
 
     return { seconds: nextSecs };
   }
 
-  function handleAnimation(currEl, nextEl, currValEl, nextValEl, nextHtml, afterNextHtml) {
+  function animate(
+    currEl,
+    nextEl,
+    currValEl,
+    nextValEl,
+    nextHtml,
+    afterNextHtml
+  ) {
     currEl.classList.add("animating");
     nextEl.classList.add("animating");
 
-    currSec.addEventListener(
+    currEl.addEventListener(
       "animationend",
       () => {
         currEl.classList.remove("animating");
@@ -80,7 +181,7 @@ const timerCallback = (function () {
       { once: true }
     );
 
-    nextSec.addEventListener(
+    nextEl.addEventListener(
       "animationend",
       () => {
         nextEl.classList.remove("animating");
@@ -92,14 +193,22 @@ const timerCallback = (function () {
 
   return () => {
     const { seconds } = handleSeconds();
+    if (!seconds) {
+      return;
+    }
+
     const { minutes, update: minutesUpdated } = handleMinutes(seconds);
 
     if (minutesUpdated) {
-      // const { hours, updateHours: update } = handleHours(minutes);
+      const { hours, update: hoursUpdated } = handleHours(minutes);
+
+      if (hoursUpdated) {
+        handleDays(hours);
+      }
     }
   };
 })();
 
-setInterval(() => {
+const intervalId = setInterval(() => {
   timerCallback();
 }, 1000);
